b833381b509fd1f230177b66c2648a4650837425
GOURAVSINGH19 <gouravsingh4495@example.com>
2024-04-12 12:57:41 +0530

    page-6

--- project-6/src/App.jsx
+++ project-6/src/App.jsx
@@ -1,9 +1,40 @@
+import { useState } from "react";
+
+const options = ["cricket","football","hockey"];
+const days = ["weekly","monthly"];
+
 function App() {
-  
+  const [game,setgame] =useState("")
+  const [day,setday] =useState("")
+
   return (
     <>
       <div>
-
+        <h1>games</h1>
+        {
+          options.map((item, index) => (
+            <div key={index}>
+            <input type="radio" value={item} id="{item}" name="options" onChange={(e)=>{
+             setgame(e.target.value)
+            }} />
+            {item}
+            </div>
+          ))
+        }
+        <h1>months</h1>
+        <div>
+        {
+          days.map((item, index) => (
+            <div  key={index}>
+            <input type="radio" value={item}  id="{item}" name="months" onChange={(e)=>{
+              setday(e.target.value)
+            }} />
+            {item}
+            </div>
+          ))
+        }
+        </div>
+        <h1> {game} on {day}</h1>
       </div>
     </>
   )
